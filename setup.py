d8be87dfd27fa2ab5f91a995a757d9b00e43b9c5
shubhamcxz <shubhamcxz@gmail.com>
2018-06-28 22:42:16 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 28 21:15:01 2018
+
+@author: shubham
+"""
+
+from setuptools import setup
+
+setup(name='themodulex',
+      version='0.1',
+      description='Common utility files for JSON manipulation.',
+      url='http://github.com/shubhamcxz',
+      author='Shubham CXZ',
+      author_email='shubhamcxz@gmail.com',
+      license='MIT',
+      packages=['themodulex'],
+      username="shubhamcxz",
+      zip_safe=False)
---
+++ themodulex/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 28 21:14:43 2018
+
+@author: shubham
+"""
+
+from .content import get_latin_text as latin
+
+def print_latin():
+    print(latin())
