6a55a44cf8b6bb3d1042e8214dcac14c9c24972b
shubhamcxz <shubhamcxz@gmail.com>
2018-06-30 10:41:46 +0530

    modified .gitignore

--- themodulex/content.py
+++ themodulex/content.py
@@ -1,15 +1,9 @@
 #Some text materials
 import numpy as np
 def get_latin_text(verbose=False):
-    text = "Lorem ipsum dolor sit amet, vim malorum dissentiet ut, tollit laudem albucius qui ut. Eam clita vidisse et. Vix scriptorem omittantur ut. Mel in veritus percipit.
-
-Ei vix oratio ceteros. Urbanitas constituam qui ne, sed an habeo lobortis. Vim et etiam quidam, fabulas molestiae sea at. Ex timeam ullamcorper mei.
-
-Quaestio sadipscing et mei, agam nibh choro vis ea. Quo ea dico commodo aliquando, mei in rebum ubique, ea sea euismod explicari. Per dicant meliore eu, ex feugait intellegat est. Est doming dicunt rationibus ut, aliquid inimicus iracundia nec no. Novum audire quaestio mel ea. Graecis nominavi offendit ut eum.
-
-Nibh principes voluptatum pro id, in pro aliquip iuvaret alienum, commune lobortis salutatus mea no. Usu at utinam ponderum percipitur. Ei nam sonet facete persequeris, eum facer causae ex. Civibus qualisque te quo,
- veri augue referrentur pri te, vim verear meliore ex. Congue vituperata vim id.
-"
+    text = "Lorem ipsum dolor sit amet, vim malorum dissentiet ut, tollit laudem albucius qui ut. Eam clita vidisse et. Vix scriptorem omittantur ut. Mel in veritus percipit. Ei vix oratio ceteros. Urbanitas constituam qui ne, sed an habeo lobortis. Vim et etiam quidam, fabulas molestiae sea at. Ex timeam ullamcorper mei.\
+Quaestio sadipscing et mei, agam nibh choro vis ea. Quo ea dico commodo aliquando, mei in rebum ubique, ea sea euismod explicari. Per dicant meliore eu, ex feugait intellegat est. Est doming dicunt rationibus ut, aliquid inimicus iracundia nec no. Novum audire quaestio mel ea. Graecis nominavi offendit ut eum.\
+Nibh principes voluptatum pro id, in pro aliquip iuvaret alienum, commune lobortis salutatus mea no. Usu at utinam ponderum percipitur. Ei nam sonet facete persequeris, eum facer causae ex. Civibus qualisque te quo, veri augue referrentur pri te, vim verear meliore ex. Congue vituperata vim id."
     if (verbose):
         print(text)
     return text
